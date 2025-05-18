97e69352298c6434d84055767f1f75eace423230
maituan-son <127265411+sonmtph23495@users.noreply.github.com>
2025-05-19 02:14:59 +0700

    task45

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,108 @@
-import { useState } from 'react'
-import { students } from './dataStudents.js'
+// import { useState } from 'react'
+// import { students } from './dataStudents.js'
+import Task45 from './component/task45'
+
 import './App.css'
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
-  const [filterAverage, setFilterAverage] = useState('all')
-  const [filterGrade, setFilterGrade] = useState('all')
+  // const [searchTerm, setSearchTerm] = useState('')
+  // const [filterAverage, setFilterAverage] = useState('all')
+  // const [filterGrade, setFilterGrade] = useState('all')
 
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value.toLowerCase())
-  }
+  // const handleChange = (e) => {
+  //   setSearchTerm(e.target.value.toLowerCase())
+  // }
 
-  const getAverage = (student) =>
-    (student.math + student.literature + student.english) / 3
+  // const getAverage = (student) =>
+  //   (student.math + student.literature + student.english) / 3
 
-  const getGrade = (avg) => {
-    if (avg >= 8) return 'Giỏi'
-    else if (avg >= 6.5) return 'Khá'
-    else if (avg >= 5) return 'Trung Bình'
-    else return 'Yếu'
-  }
+  // const getGrade = (avg) => {
+  //   if (avg >= 8) return 'Giỏi'
+  //   else if (avg >= 6.5) return 'Khá'
+  //   else if (avg >= 5) return 'Trung Bình'
+  //   else return 'Yếu'
+  // }
 
-  const filteredStudents = students.filter((student) => {
-    const nameMatch = student.name.toLowerCase().includes(searchTerm)
-    const average = getAverage(student)
-    const grade = getGrade(average)
+  // const filteredStudents = students.filter((student) => {
+  //   const nameMatch = student.name.toLowerCase().includes(searchTerm)
+  //   const average = getAverage(student)
+  //   const grade = getGrade(average)
 
-    const averageMatch =
-      filterAverage === 'all' ||
-      (filterAverage === '>=8' && average >= 8) ||
-      (filterAverage === '6.5-8' && average >= 6.5 && average < 8) ||
-      (filterAverage === '5-6.5' && average >= 5 && average < 6.5) ||
-      (filterAverage === '<5' && average < 5)
+  //   const averageMatch =
+  //     filterAverage === 'all' ||
+  //     (filterAverage === '>=8' && average >= 8) ||
+  //     (filterAverage === '6.5-8' && average >= 6.5 && average < 8) ||
+  //     (filterAverage === '5-6.5' && average >= 5 && average < 6.5) ||
+  //     (filterAverage === '<5' && average < 5)
 
-    const gradeMatch = filterGrade === 'all' || grade === filterGrade
+  //   const gradeMatch = filterGrade === 'all' || grade === filterGrade
 
-    return nameMatch && averageMatch && gradeMatch
-  })
-
+  //   return nameMatch && averageMatch && gradeMatch
+  // })
   return (
-    <div className="App">
-      <h1>Danh sách sinh viên</h1>
+  <>
+<div>
+  <Task45 />
+</div>
+  </>
+    // <div className="App">
+    //   <h1>Danh sách sinh viên</h1>
 
-      <div style={{ marginBottom: '10px' }}>
-        <input
-          type="text"
-          onChange={handleChange}
-          placeholder="Nhập tên sinh viên"
-        />
+    //   <div style={{ marginBottom: '10px' }}>
+    //     <input
+    //       type="text"
+    //       onChange={handleChange}
+    //       placeholder="Nhập tên sinh viên"
+    //     />
 
-        <select onChange={(e) => setFilterAverage(e.target.value)}>
-          <option value="all">Tất cả điểm TB</option>
-          <option value=">=8">Điểm TB ≥ 8</option>
-          <option value="6.5-8">6.5 ≤ Điểm TB &lt; 8</option>
-          <option value="5-6.5">5 ≤ Điểm TB &lt; 6.5</option>
-          <option value="<5">Điểm TB &lt; 5</option>
-        </select>
+    //     <select onChange={(e) => setFilterAverage(e.target.value)}>
+    //       <option value="all">Tất cả điểm TB</option>
+    //       <option value=">=8">Điểm TB ≥ 8</option>
+    //       <option value="6.5-8">6.5 ≤ Điểm TB &lt; 8</option>
+    //       <option value="5-6.5">5 ≤ Điểm TB &lt; 6.5</option>
+    //       <option value="<5">Điểm TB &lt; 5</option>
+    //     </select>
 
-        <select onChange={(e) => setFilterGrade(e.target.value)}>
-          <option value="all">Tất cả học lực</option>
-          <option value="Giỏi">Giỏi</option>
-          <option value="Khá">Khá</option>
-          <option value="Trung Bình">Trung Bình</option>
-          <option value="Yếu">Yếu</option>
-        </select>
-      </div>
+    //     <select onChange={(e) => setFilterGrade(e.target.value)}>
+    //       <option value="all">Tất cả học lực</option>
+    //       <option value="Giỏi">Giỏi</option>
+    //       <option value="Khá">Khá</option>
+    //       <option value="Trung Bình">Trung Bình</option>
+    //       <option value="Yếu">Yếu</option>
+    //     </select>
+    //   </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Tên</th>
-            <th>Toán</th>
-            <th>Văn</th>
-            <th>Tiếng Anh</th>
-            <th>Điểm TB</th>
-            <th>Học Lực</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredStudents.map((student) => {
-            const avg = getAverage(student).toFixed(2)
-            const grade = getGrade(avg)
-            return (
-              <tr key={student.id}>
-                <td>{student.id}</td>
-                <td>{student.name}</td>
-                <td>{student.math}</td>
-                <td>{student.literature}</td>
-                <td>{student.english}</td>
-                <td>{avg}</td>
-                <td>{grade}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+    //   <table>
+    //     <thead>
+    //       <tr>
+    //         <th>ID</th>
+    //         <th>Tên</th>
+    //         <th>Toán</th>
+    //         <th>Văn</th>
+    //         <th>Tiếng Anh</th>
+    //         <th>Điểm TB</th>
+    //         <th>Học Lực</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       {filteredStudents.map((student) => {
+    //         const avg = getAverage(student).toFixed(2)
+    //         const grade = getGrade(avg)
+    //         return (
+    //           <tr key={student.id}>
+    //             <td>{student.id}</td>
+    //             <td>{student.name}</td>
+    //             <td>{student.math}</td>
+    //             <td>{student.literature}</td>
+    //             <td>{student.english}</td>
+    //             <td>{avg}</td>
+    //             <td>{grade}</td>
+    //           </tr>
+    //         )
+    //       })}
+    //     </tbody>
+    //   </table>
+    // </div>
   )
 }
 
---
+++ src/component/task45.jsx
@@ -0,0 +1,168 @@
+import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* 
+Gọi dữ liệu products từ "https://dummyjson.com/products"
+Danh sách sản phẩm mặc định với giá trị mặc định là page = 1, limit = 12
+Tạo tính năng phân trang với nút next, preview (khuyến khích sử dụng icon cho giao diện)
+Có thể thay đổi được số lượng sản phẩm hiển thị trên 1 trang với select...option.
+Tạo tính năng sắp xếp sản phẩm theo giá từ thấp đến cao, từ cao đến thấp, hủy sắp xếp.
+Tạo tính năng tìm kiếm theo tên sản phẩm (không phân biệt hoa thường)
+Deploy lên vercel và nộp lại link deploy.
+ */
+const url = 'https://dummyjson.com/products'
+
+
+
+const Task45 = () => {
+    const [products, setProducts] = useState([])
+    const [page, setPage] = useState(1)
+    const [seach , setSeach] = useState('')
+    const [sort , setSort] = useState('default')
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch(`${url}?limit=12&skip=${(page - 1) * 12}`)
+            const data = await response.json()
+            setProducts(data.products)
+        }
+        fetchProducts()
+    }, [page])
+    const handlePage = (page) => {
+        if (page === 'prev') {
+            setPage((prevPage) => Math.max(prevPage - 1, 1))
+        } else if (page === 'next') {
+            setPage((prevPage) => prevPage + 1)
+        }
+    }
+   const handleChangeLimit = (e) => {
+        const limit = e.target.value
+        const fetchProducts = async () => {
+            const response = await fetch(`${url}?limit=${limit}&skip=${(page - 1) * limit}`)
+            const data = await response.json()
+            setProducts(data.products)
+        }
+        fetchProducts()
+    }
+    const handleSort = (sort) => {
+        setSort(sort)
+        if (sort === 'asc') {
+            const sortedProducts = [...products].sort((a, b) => a.price - b.price)
+            setProducts(sortedProducts)
+        } else if (sort === 'desc') {
+            const sortedProducts = [...products].sort((a, b) => b.price - a.price)
+            setProducts(sortedProducts)
+        } else {
+            const fetchProducts = async () => {
+                const response = await fetch(`${url}?limit=12&skip=${(page - 1) * 12}`)
+                const data = await response.json()
+                setProducts(data.products)
+            }
+            fetchProducts()
+        }
+    }
+    const handlseach = (e) => {
+        setSeach(e.target.value)
+    }
+    const filteredProducts = products.filter((product) =>
+        //toán tử 3 ngôi nếu chuối rỗng thì không tìm kiếm 
+        product.title.toLowerCase().includes(seach.toLowerCase())
+       
+    )
+    
+
+
+
+  return (
+  <div className="container mt-5">
+  <h1 className="mb-4 text-center">Danh sách sản phẩm</h1>
+
+  {/* Search + Sort */}
+  <div className="row mb-3">
+    <div className="col-md-6">
+      <input
+        type="text"
+        onChange={handlseach}
+        placeholder="Tìm kiếm sản phẩm"
+        className="form-control"
+      />
+    </div>
+    <div className="col-md-6 d-flex align-items-center gap-2">
+      <button
+        onClick={() => setProducts(filteredProducts)}
+        className="btn btn-primary w-160"
+      >
+        Tìm kiếm
+      </button>
+    </div>
+
+  </div>
+
+  {/* Product Table */}
+  <div className="table-responsive">
+    <table className="table table-bordered table-hover">
+      <thead className="table-light">
+        <tr>
+          <th>ID</th>
+          <th>Tên</th>
+          <th>Giá  <select
+        id="sort"
+        onChange={(e) => handleSort(e.target.value)}
+        className="form-select"
+      >
+        <option value="default">Mặc định</option>
+        <option value="asc">Tăng dần</option>
+        <option value="desc">Giảm dần</option>
+      </select></th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td>{product.id}</td>
+            <td>{product.title}</td>
+            <td>{product.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
+  {/* Pagination & Page Size */}
+  <div className="row align-items-center mt-4">
+    <div className="col-md-6 d-flex align-items-center gap-2">
+      <button
+        className="btn btn-secondary"
+        onClick={() => handlePage("prev")}
+        disabled={page === 1}
+      >
+        Prev
+      </button>
+      <span className="mx-2">Trang {page}</span>
+      <button className="btn btn-secondary" onClick={() => handlePage("next")}>
+        Next
+      </button>
+    </div>
+    <div className="col-md-6 text-md-end mt-3 mt-md-0">
+      <label htmlFor="limit" className="form-label me-2">
+        Số lượng / trang:
+      </label>
+      <select
+        id="limit"
+        onChange={handleChangeLimit}
+        className="form-select d-inline-block w-auto"
+      >
+        <option value="12">12</option>
+        <option value="24">24</option>
+        <option value="36">36</option>
+        <option value="48">48</option>
+        <option value="60">60</option>
+        <option value="194">Tất cả</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+  )
+}
+
+export default Task45
